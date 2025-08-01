d5dd9e86372f784dd83e4e9fa80ec2c809a8c011
Emina0202 <emina89637979259@gmail.com>
2025-08-02 01:14:11 +0300

    commit-m

--- 06_lesson/lesson06_task1.py
+++ 06_lesson/lesson06_task1.py
@@ -3,8 +3,9 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=ChromeService(
-ChromeDriverManager().install()))
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install())
+    )
 driver.implicitly_wait(20)
 
 driver.get('http://uitestingplayground.com/ajax')
