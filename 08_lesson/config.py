cc1f80fbc48a270bcb8bd565659c6dbbd4ef7548
Emina0202 <emina89637979259@gmail.com>
2025-08-23 02:45:57 +0300

    commit-m 08_lesson

--- 08_lesson/config.py
+++ 08_lesson/config.py
@@ -2,7 +2,6 @@ class Config:
     BASE_URL = "https://ru.yougile.com"
     API_URL = f"{BASE_URL}/api-v2"
 
-    # Эти значения нужно заменить на реальные данные
     USERNAME = "emina89637979259@gmail.com"
     PASSWORD = "umar/2020/"
     API_TOKEN = "basic_auth_token"
