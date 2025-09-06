c2dee124da9d692465d34ce4c0f71ff2c7ce4ff9
Emina0202 <emina89637979259@gmail.com>
2025-09-06 11:53:47 +0300

    commit-m

--- 08_lesson/test_posit.py
+++ 08_lesson/test_posit.py
@@ -9,144 +9,101 @@ def get_token():
     auth_data = {
         "login": "emina89637979259@gmail.com",
         "password": "umar/2020/",
-        "companyId": "9347006b-dc75-4550-97d5-3008ba00d4a0"  # ID компании
+        "companyId": "c0b180aa-0d96-4289-9753-c9b08950859f"
     }
 
-    headers = {'Content-Type': 'application/json'}  # Заголовки HTTP-запроса
+    headers = {'Content-Type': 'application/json'}
 
-# Отправляем POST-запрос на сервер для получения ключа авторизации
+    # Отправляем POST-запрос на сервер для получения ключа авторизации
     response = requests.post(
         'https://ru.yougile.com/api-v2/auth/keys',
-        data=json.dumps(auth_data),  # Преобразование данных в JSON
-        headers=headers              # Передача заголовков
+        data=json.dumps(auth_data),
+        headers=headers
     )
     token = response.json()['key']
     yield token
 
 
- # Фикстура для получения списка пользователей
-
-
+# Фикстура для получения списка пользователей
 @pytest.fixture(scope="module")
 def get_user_list(get_token):
-    headers = {'Content-Type': 'application/json',
-               'Authorization': f'Bearer LMO2a+0uZ5mfMmU2wUFLvOx3Lmckd77jrKVc6AvNs1PUOlc5JNzeG12ysmwur7aj'  # Заголовки с токеном
-               }                                       # авторизации
+    token = "rL6IrqOzW66+Fwx+68Aqda4X+980EKThSc93vkrjkih6jEUFnrfF2BoI7ejT3Ixo"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
 
-# Отправляем GET-запрос для получения списка пользователей
+    # Отправляем GET-запрос для получения списка пользователей
     response = requests.get(
         'https://ru.yougile.com/api-v2/users',
         headers=headers
     )
-    print(response.json())  # Печать полученного ответа (для дебага)
-    # return response.json()  # Возврат списка пользователей
-    get_user_list(
-        'LMO2a+0uZ5mfMmU2wUFLvOx3Lmckd77jrKVc6AvNs1PUOlc5JNzeG12ysmwur7aj')
+    print(response.json())
+    return response.json()
 
 
 # Тест на создание нового проекта
 def test_create_project(get_token):
-    project_data = {
-  "title": "ГосУслуги",
-  "users": {
-    "4902b994-b806-4af4-acec-018ea5ea6468": "worker",
-    "8aeaeb9d-f94e-4c66-96d3-eb8d96fe7018": "ee88efd5-5cb2-41a0-9ea2-295da25863d4"
-  }
-}
+    project_data = {"title": "новый проект для тестирования"}
 
-# Заголовки с токеном авторизации
-    headers = {'Content-Type': 'application/json',
-               'Authorization': f'Bearer LMO2a+0uZ5mfMmU2wUFLvOx3Lmckd77jrKVc6AvNs1PUOlc5JNzeG12ysmwur7aj'}
+    token = "rL6IrqOzW66+Fwx+68Aqda4X+980EKThSc93vkrjkih6jEUFnrfF2BoI7ejT3Ixo"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
 
-# Отправляем POST-запрос для создания проекта
+    # Отправляем POST-запрос для создания проекта
     response = requests.post(
         'https://ru.yougile.com/api-v2/projects',
-        data=json.dumps(project_data),  # Данные проекта в виде JSON
-        headers=headers                 # Заголовки запроса
+        data=json.dumps(project_data),
+        headers=headers
     )
-    # Печать результата операции (для дебага)
-    print(response.json())
 
-    # Утверждение успешного статуса ответа (ожидаем 201 Created)
+    print(response.json())
     assert response.status_code == 201
+
 
 # Тест на обновление существующего проекта
 def test_edit_project(get_token):
     edit_data = {
-        "deleted": False,              # Проект не удалён
-        "title": "ГосУслуги1",         # Новое название проекта
-        "users": {
-    "4902b994-b806-4af4-acec-018ea5ea6468": "worker",
-    "8aeaeb9d-f94e-4c66-96d3-eb8d96fe7018": "ee88efd5-5cb2-41a0-9ea2-295da25863d4"
-  }
+        "deleted": False,
+        "title": "новый проект для тестирования1"
     }
-    # Заголовки с токеном авторизации
-    headers = {'Content-Type': 'application/json',
-               'Authorization': f'Bearer LMO2a+0uZ5mfMmU2wUFLvOx3Lmckd77jrKVc6AvNs1PUOlc5JNzeG12ysmwur7aj'}
+
+    token = "rL6IrqOzW66+Fwx+68Aqda4X+980EKThSc93vkrjkih6jEUFnrfF2BoI7ejT3Ixo"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
 
     # Отправляем PUT-запрос для обновления проекта
     response = requests.put(
         'https://ru.yougile.com/api-v2/projects/'
-        '003400df-16f3-4452-83f9-f5e03d48f5cd',
-        data=json.dumps(edit_data),     # Обновленные данные проекта
-        headers=headers                 # Заголовки запроса
+        '805a7cc8-9466-4593-a8d8-5543d4ba1a6c',
+        data=json.dumps(edit_data),
+        headers=headers
     )
-    # Печать результата операции (для дебага)
-    print(response.json())
 
-    # Утверждение успешного статуса ответа (ожидаем 200 OK)
+    print(response.json())
     assert response.status_code == 200
 
 
-# Тест на связывание проекта с пользователями
+# Тест на получение проекта по ID
 def test_bind_by_id(get_token):
-    auth_data = {
-        "title": "ГосУслуги1",  # Название проекта
-        "users": {
-    "4902b994-b806-4af4-acec-018ea5ea6468": "worker",
-    "8aeaeb9d-f94e-4c66-96d3-eb8d96fe7018": "ee88efd5-5cb2-41a0-9ea2-295da25863d4"
-  }
+    auth_data = {"title": "новый проект для тестирования1"}
+
+    token = "rL6IrqOzW66+Fwx+68Aqda4X+980EKThSc93vkrjkih6jEUFnrfF2BoI7ejT3Ixo"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
     }
-    # Заголовки с токеном авторизации
-    headers = {'Content-Type': 'application/json',
-               'Authorization': f'Bearer LMO2a+0uZ5mfMmU2wUFLvOx3Lmckd77jrKVc6AvNs1PUOlc5JNzeG12ysmwur7aj'}
-    # Отправляем POST-запрос для связывания проекта с пользователями
+
+    # Отправляем GET-запрос для получения проекта
     response = requests.get(
         'https://ru.yougile.com/api-v2/projects/'
-        '003400df-16f3-4452-83f9-f5e03d48f5cd',
-        data=json.dumps(auth_data),             # Данные проекта
-        headers=headers                         # Заголовки запроса
-
+        '805a7cc8-9466-4593-a8d8-5543d4ba1a6c',
+        headers=headers
     )
-    # Печать результата операции (для дебага)
+
     print(response.json())
-
-    # Утверждение успешного статуса ответа (ожидаем 201 Created)
-
-    assert response.status_code == 200
-
-
-def test_delete_project(get_token):
-    project_data = {
-        "title": "ГосУслуги",  # Название проекта
-        "users": {
-    "4902b994-b806-4af4-acec-018ea5ea6468": "worker",
-    "8aeaeb9d-f94e-4c66-96d3-eb8d96fe7018": "ee88efd5-5cb2-41a0-9ea2-295da25863d4"
-  }
-    }
-
-# Заголовки с токеном авторизации
-    headers = {'Content-Type': 'application/json',
-               'Authorization': f'Bearer LMO2a+0uZ5mfMmU2wUFLvOx3Lmckd77jrKVc6AvNs1PUOlc5JNzeG12ysmwur7aj'}
-
-# Отправляем POST-запрос для создания проекта
-    response = requests.post(
-        'https://ru.yougile.com/api-v2/auth/keys/{key}',
-        data=json.dumps(project_data),  # Данные проекта в виде JSON
-        headers=headers                 # Заголовки запроса
-    )
-    # Печать результата операции (для дебага)
-    print(response.json())
-
-    # Утверждение успешного статуса ответа (ожидаем 201 Created)
     assert response.status_code == 200
